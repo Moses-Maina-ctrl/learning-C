5b92b8d5db42ee4c949b5f3945a5834c4cadbbba
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-06-17 23:51:44 +0300

    Single Dimensional Array

---
+++ singleDimensionalArray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void times2(int *a, int len)
+{
+  for (int i =0, i < len, i++)
+  {
+    printf("%d\n", a[i] *2);
+  }
+}
+
+void times3(int a[], int len)
+{
+  for (int i =0, i < len, i++)
+  {
+    printf("%d\n", a[i]*3);
+  }
+}
+
+void times4(int a[5], int len)
+{
+  for (int i = 0, i < len, i++)
+  {
+    printf("%d/n", a[i] * 4);
+  }
+}
+
+int main(void){
+  int x[5] = {11, 22, 33, 44, 55};
+  int len = sizeof(x) / sizeof(int)
+
+  times2(x, len)
+  times3(x, len)
+  times4(x, len)
+}
