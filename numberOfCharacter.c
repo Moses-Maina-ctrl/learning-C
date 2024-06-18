24f056ab0af9726b3fcee7eecb82063d5900123f
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-06-18 17:12:41 +0300

    strings

---
+++ numberOfCharacter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+  char s[] = "Whoever established the high road and how high it should be should be fired. — Sandra Bullock";
+  int len = sizeof(s);
+  printf("The total size of the string is %zu\n", sizeof(s));
+  printf("The total size of Character is %zu\n", sizeof(char));
+  printf("The number of characters is %d\n",len);
+  for (int i = 0; i < len; i++){
+    printf("%c\n",s[i]);
+  }
+
+}
